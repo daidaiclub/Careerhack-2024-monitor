f230f09cd097414ba1f2d4afec458352e6ca96ca
jimmyhealer <yahing6066@gmail.com>
2024-01-23 16:39:28 +0800

    feat: add twice error condiction and detect error metric within 5 miniutes

--- genAI/main.py
+++ genAI/main.py
@@ -36,9 +36,21 @@ def simulate_realtime_csv():
 
 # {'Container CPU Utilization (%)': 16.0875, 'Container Memory Utilization (%)': 56.99, 'Container Startup Latency (ms)': nan, 'Instance Count (active)': 1.0, 'Instance Count (idle)': 1.0, 'Request Count (1xx)': 0.0, 'Request Count (2xx)': 5.0, 'Request Count (3xx)': 0.0, 'Request Count (4xx)': 4.0, 'Request Latency (ms)': 31.2701591}
 # 檢查指標是否異常
+cpu_error_times = 0
+memory_error_times = 0
+
 def check_metrics_abnormalities(metrics):
-  if metrics['Container CPU Utilization (%)'] > 60 or metrics['Container Memory Utilization (%)'] > 60:
-    return True
+  if metrics['Container CPU Utilization (%)'] > 60:
+    global cpu_error_times
+    cpu_error_times += 1
+    if cpu_error_times >= 2:
+      return True
+  
+  if metrics['Container Memory Utilization (%)'] > 80:
+    global memory_error_times
+    memory_error_times += 1
+    if memory_error_times >= 2:
+      return True
   
   if metrics['Container Startup Latency (ms)'] > 0:
     return True
@@ -50,10 +62,12 @@ def check_metrics_abnormalities(metrics):
     return True
   
   return False
-  
+
+metrics_data = []
+
 if __name__ == '__main__':
   for i in simulate_realtime_csv():
-    print(i)
+    metrics_data.append(i)
     if check_metrics_abnormalities(i):
-      gen_solution('', str(i))
-      # break
+      gen_solution('', str(metrics_data[-5:]))
+      break
